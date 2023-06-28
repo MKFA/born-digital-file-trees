c07feec4634d14eaa0c6f2857158acedf6b6ddae
Brock Stuessi <bstuessi@mikekelleyfoundation.org>
2023-06-28 12:18:18 -0700

    add password landing page

---
+++ script.js
@@ -0,0 +1,18 @@
+// Define the correct password
+const correctPassword = "poltergeist";
+
+// Handle form submission
+document.getElementById("passwordForm").addEventListener("submit", function(event) {
+  event.preventDefault(); // Prevent form submission
+
+  // Get the entered password
+  const password = document.getElementById("passwordInput").value;
+
+  // Check if the entered password matches the correct password
+  if (password === correctPassword) {
+    // Redirect to the protected content
+    window.location.href = "potential-muller-disks.html";
+  } else {
+    alert("Incorrect password. Please try again.");
+  }
+});
